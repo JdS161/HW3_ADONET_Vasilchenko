7bfb73d96baf7a732656a24368bb9147b39bf23f
JdS161 <a.vasiltschenko@gmail.com>
2022-08-20 16:46:08 +0400

    Add project files.

---
+++ HW3_ADONET_Vasilchenko/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace HW3_ADONET_Vasilchenko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.selectOneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPictureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.selectAllToolStripMenuItem,
+            this.toolStripTextBox1,
+            this.selectOneToolStripMenuItem,
+            this.insertToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.updateToolStripMenuItem,
+            this.addPictureToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1674, 37);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // selectAllToolStripMenuItem
+            // 
+            this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(97, 31);
+            this.selectAllToolStripMenuItem.Text = "Show All";
+            this.selectAllToolStripMenuItem.Click += new System.EventHandler(this.selectAllToolStripMenuItem_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 31);
+            this.toolStripTextBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.toolStripTextBox1_KeyPress);
+            // 
+            // selectOneToolStripMenuItem
+            // 
+            this.selectOneToolStripMenuItem.Name = "selectOneToolStripMenuItem";
+            this.selectOneToolStripMenuItem.Size = new System.Drawing.Size(110, 31);
+            this.selectOneToolStripMenuItem.Text = "Show One";
+            this.selectOneToolStripMenuItem.Click += new System.EventHandler(this.selectOneToolStripMenuItem_Click);
+            // 
+            // insertToolStripMenuItem
+            // 
+            this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(72, 31);
+            this.insertToolStripMenuItem.Text = "Insert";
+            this.insertToolStripMenuItem.Click += new System.EventHandler(this.insertToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(78, 31);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(86, 31);
+            this.updateToolStripMenuItem.Text = "Update";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
+            // addPictureToolStripMenuItem
+            // 
+            this.addPictureToolStripMenuItem.Name = "addPictureToolStripMenuItem";
+            this.addPictureToolStripMenuItem.Size = new System.Drawing.Size(120, 31);
+            this.addPictureToolStripMenuItem.Text = "Add Picture";
+            this.addPictureToolStripMenuItem.Click += new System.EventHandler(this.addPictureToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1308, 502);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1326, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(337, 502);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1650, 35);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1674, 593);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem selectOneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addPictureToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ HW3_ADONET_Vasilchenko/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace HW3_ADONET_Vasilchenko
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["CompanyDB"].ConnectionString;
+        SqlConnection conn;
+        SqlDataAdapter adapter;
+        DataSet ds;
+        SqlCommandBuilder command;
+        string fileName = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void addPictureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Graphics File |*.jpeg; *.jpg; *.bmp; *.png; *.gif";
+            ofd.FileName = "";
+            if(ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = ofd.FileName;
+                LoadPicture();
+            }
+        }
+
+        private void LoadPicture()
+        {
+            try
+            {
+                byte[] bytes = CreateCopy();
+                conn.Open();
+                SqlCommand comm = new SqlCommand("Insert into EmployeePictures(EmployeeID, FilePath, Picture) VALUES (@EmployeeID, @FilePath, @Picture)", conn);
+                if (toolStripTextBox1.Text == null || toolStripTextBox1.Text.Length == 0) return;
+                int index = -1;
+                int.TryParse(toolStripTextBox1.Text, out index);
+                if (index == -1) return;
+                comm.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = index;
+                comm.Parameters.Add("@FilePath", SqlDbType.NVarChar, 255).Value = fileName;
+                comm.Parameters.Add("@Picture", SqlDbType.Image, bytes.Length).Value = bytes;
+                comm.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+        }
+
+        private byte[] CreateCopy()
+        {
+            Image img = Image.FromFile(fileName);
+            int maxWidth = 300, maxHeight = 300;
+
+            double ratioX = (double)maxWidth / img.Width;
+            double ratioY = (double)maxHeight / img.Height;
+            double ratio = Math.Min(ratioX, ratioY);
+            int newWidth = (int)(img.Width*ratio);
+            int newHeight = (int)(img.Height*ratio);
+            Image mi = new Bitmap(newWidth, newHeight);
+
+            Graphics g = Graphics.FromImage(mi);
+            g.DrawImage(img, 0, 0, newWidth, newHeight);
+            MemoryStream ms = new MemoryStream();
+
+            mi.Save(ms, ImageFormat.Jpeg);
+            ms.Flush();
+            ms.Seek(0, SeekOrigin.Begin);
+            BinaryReader br = new BinaryReader(ms); 
+            byte[] buf = br.ReadBytes((int)ms.Length);
+            return buf;
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "SELECT DISTINCT Employee.EmployeeID, FirstName, LastName, BirthDate, EmployeePictures.Picture AS N'Picture' FROM Employee, EmployeePictures WHERE Employee.EmployeeID = EmployeePictures.EmployeeID";
+                adapter = new SqlDataAdapter(query, conn);
+                SqlCommandBuilder cmb = new SqlCommandBuilder(adapter);
+                ds = new DataSet();
+                adapter.Fill(ds, "Picture");
+                dataGridView1.DataSource = ds.Tables["Picture"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void selectOneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(toolStripTextBox1.Text == null || toolStripTextBox1.Text.Length == 0)
+                {
+                    MessageBox.Show("Write down the number of Employee you want to look at");
+                    return;
+                }
+
+                int index = -1;
+                int.TryParse(toolStripTextBox1.Text, out index);
+                if(index == -1)
+                {
+                    MessageBox.Show("Invalid Employee ID");
+                    return ;
+                }
+
+                adapter = new SqlDataAdapter("SELECT EmployeeID, FirstName, LastName, BirthDate, PositionID, EmployeePicture FROM Employee WHERE EmployeeID = @EmployeeID", conn);
+                SqlCommandBuilder cmb = new SqlCommandBuilder(adapter);
+                adapter.SelectCommand.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = index;
+                ds = new DataSet();
+                adapter.Fill(ds, "Employee");
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void toolStripTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar <=48 || e.KeyChar>=59) && e.KeyChar !=8)
+                e.Handled = true;
+        }
+
+        private void insertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = new DataSet();
+                adapter = new SqlDataAdapter(textBox1.Text, conn);
+                dataGridView1.DataSource = null;
+                command = new SqlCommandBuilder(adapter);
+                adapter.Fill(ds, "Employee");
+                dataGridView1.DataSource = ds.Tables["Employee"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            selectAllToolStripMenuItem_Click(sender, e);
+        }
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                command = new SqlCommandBuilder(adapter);
+                ds = new DataSet();
+                adapter.Fill(ds, "Employee");
+                adapter.Update(ds, "Employee");
+                adapter.UpdateCommand = command.GetUpdateCommand();
+                adapter.Fill(ds, "Employee");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {            
+            try 
+            {
+                if (toolStripTextBox1.Text == null || toolStripTextBox1.Text.Length == 0)
+                {
+                    MessageBox.Show("Write down the number of Employee you want to look at");
+                    return;
+                }
+
+                int index = -1;
+                int.TryParse(toolStripTextBox1.Text, out index);
+                if (index == -1)
+                {
+                    MessageBox.Show("Invalid Employee ID");
+                    return;
+                }
+
+                DialogResult dr;
+                dr = MessageBox.Show("Are you sure?\nImpossible undo this command while executed", "Deletion confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (dr == DialogResult.Yes)
+                {
+                    adapter.DeleteCommand = new SqlCommand("DELETE FROM Employee WHERE EmployeeID = @EmployeeID", conn);
+                    adapter.DeleteCommand.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = index;
+                    conn.Open();
+                    adapter.DeleteCommand.ExecuteNonQuery();
+                    conn.Close();
+                    ds.Clear();
+                    adapter.Fill(ds);
+                }
+                else
+                {
+                    MessageBox.Show("Deletion cancelled");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            selectAllToolStripMenuItem_Click(sender, e);
+        }
+    }
+}
